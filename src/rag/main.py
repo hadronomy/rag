998c1dda5bebe31d2af0157bb9b9db6eb73fbd2a
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-06-10 01:39:59 +0100

    refactor: rename `app` to `cli`

--- src/rag/main.py
+++ src/rag/main.py
@@ -1,11 +1,10 @@
+from settings import get_settings
 from typer import Typer
 
-from settings import get_settings
-
-app = Typer(name="rag", no_args_is_help=True)
+cli = Typer(name="rag", no_args_is_help=True)
 
 
-@app.command("start")
+@cli.command("start")
 def start():
     """Start the RAG server."""
     settings = get_settings()
@@ -19,5 +18,5 @@ def callback():
 
 
 if __name__ == "__main__":
-    app.callback()(callback)
-    app()
+    cli.callback()(callback)
+    cli()
